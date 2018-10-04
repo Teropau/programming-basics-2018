dd68f7229e36d3d2f287029d82f4aaacd3c1ed6a
Tero <tero.pau@gmail.com>
2018-10-04 08:32:43 +0300

    RndTask1 done, while_task1 done, WhileTask2 initialized

---
+++ Do_while_for_tasks/RndTask1/RndTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RndTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä ohjelma tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret.");
+            Console.WriteLine("  Luku         Nelöjuuri");
+
+            Random rnd = new Random();
+
+            int rndNumber;
+            
+            for(int i=0; i<10; i++)
+            {
+                rndNumber = rnd.Next(0,100);
+                Console.WriteLine($"{i + 1}. {rndNumber}         {Math.Sqrt(rndNumber)}");
+            }
+           
+        }
+    }
+}
---
+++ Do_while_for_tasks/while_task1/while_task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace while_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program calculates N! of given number.");
+            Console.WriteLine("give a number.");
+
+            while (true)
+            {
+                
+
+                string input;
+                input = Console.ReadLine();
+                int number;
+                while (!Int32.TryParse(input, out number))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Don't u know what anumber is??");
+                    Console.WriteLine("give a NUMBER!      please..");
+                    input = Console.ReadLine();
+                }
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Input is invalid. Are you trying to break me? Fat chance. just restart the program.");
+                }
+
+                if(number == 0)
+                {
+                    Console.WriteLine("No zeroes please");
+                }
+
+                double i = 1;
+                double F = 1;
+                while (i != number)
+                {
+                    i = i + 1;
+                    F = F * i;
+                }
+
+                
+                Console.Clear();
+                Console.WriteLine("This program calculates N! of given number.");
+                Console.WriteLine("give a number.");
+                Console.WriteLine($"here you go: {F}");
+
+
+            }
+        }
+    }
+}
