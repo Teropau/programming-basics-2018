6363a9329093ee90b4dcc762392993ae3e8cf120
Tero <tero.pau@gmail.com>
2018-10-06 21:09:15 +0300

    Program prints 20 random numbers in 5 rows.

--- Do_while_for_tasks/loopTask4.3/loopTask4.3/Program.cs
+++ Do_while_for_tasks/loopTask4.3/loopTask4.3/Program.cs
@@ -6,7 +6,27 @@ namespace loopTask4._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa 20 satunnaista kokonaislukua, 5 per rivi.");
+
+            Random rnd = new Random();
+            int rndNumber;
+
+            for(int i=1; i!=5; i++)
+            {
+                Console.Write($"Rivi{i} :");
+
+                for(int y=0; y!=5; y++)
+                {
+                    rndNumber = rnd.Next(50);
+                    Console.Write(rndNumber);
+                    if(y!=4)
+                    {
+                        Console.Write(",");
+                    }
+                }
+
+                Console.WriteLine();  
+            }
         }
     }
 }
