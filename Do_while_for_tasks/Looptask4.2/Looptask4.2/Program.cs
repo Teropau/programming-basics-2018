c91f28cde720a4164a1e81433b171c77a3fc9ec1
Tero <tero.pau@gmail.com>
2018-10-06 10:35:16 +0300

    loopTask 4.2 initialized and done. This program prints out multiplication table.

---
+++ Do_while_for_tasks/Looptask4.2/Looptask4.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Looptask4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa lertotaulun luvuille 1-9.");
+            int i = 1;
+            int y = 1;
+            int tulo;
+
+            while(i!=10)
+            {
+                tulo = i * y;
+                Console.WriteLine($"{i} x {y} = {tulo}");
+
+                if (y == 9)
+                {
+                    y = 1;
+                    i++;
+                }
+                else
+                {
+                    y++;
+                }
+            }
+        }
+
+    }
+}
