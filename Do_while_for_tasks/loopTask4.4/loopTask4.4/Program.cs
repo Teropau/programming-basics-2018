893c6d1cdcaafd505b5ca59965b966b3eb24f84e
Tero <tero.pau@gmail.com>
2018-10-06 22:23:29 +0300

    Program flips a coin 20 times and tells how many heads and tails you got.

---
+++ Do_while_for_tasks/loopTask4.4/loopTask4.4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace loopTask4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma heittää kolikkoa 20 kerrtaa ja");
+            Console.WriteLine("kertoo, kuinka monta kruunaa ja klaavaa tuli.");
+            int heads=0;
+            int tails=0;
+
+            Random rnd = new Random();
+            double flip;
+
+            for(int i=0; i!=20; i++)
+            {
+                flip = rnd.NextDouble();
+
+                if(flip>0.5)
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+
+            }
+
+            Console.WriteLine($"Kruunia: {heads}   Klaavaoja: {tails}");
+        }
+    }
+}
