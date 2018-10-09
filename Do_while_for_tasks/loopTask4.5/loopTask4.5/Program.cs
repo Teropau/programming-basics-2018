ca1a80f799868f5e789506d53b8ef93ed37e0cc7
Tero <tero.pau@gmail.com>
2018-10-09 12:02:02 +0300

    Program prints outcomes for betting.

---
+++ Do_while_for_tasks/loopTask4.5/loopTask4.5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace loopTask4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa vakioveikkauksen rivin.");
+            Random rnd = new Random();
+
+            for(int i=1; i!=14; i++)
+            {
+                Console.Write($"{i}.  ");
+                double x = rnd.Next(100);
+
+                if (x <= 40)
+                {
+                    Console.WriteLine(1);
+                }
+                else if (x > 40 && x < 60)
+                {
+                    Console.WriteLine(2);
+                    
+                }
+                else
+                {
+                    Console.WriteLine("X");
+                }
+
+            }
+
+
+        }
+    }
+}
