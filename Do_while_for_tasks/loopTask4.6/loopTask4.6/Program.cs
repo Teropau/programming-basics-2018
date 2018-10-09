5b5cb95bd840988e94801af1fe95a014008d165d
Tero <tero.pau@gmail.com>
2018-10-09 20:51:06 +0300

    Program throws a die a thousand times and then tells how many sixes you got.

---
+++ Do_while_for_tasks/loopTask4.6/loopTask4.6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace loopTask4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma heittää noppaa 1000 kertaa jakertoo, kuinka monta kutosta tuli.");
+
+            int kutonen=0;
+
+            for(int i = 1; i!=1002; i++)
+            {
+                Random rnd = new Random();
+
+                Console.Write($"{i}. ");
+                int noppa;
+                do
+                {
+                    noppa = rnd.Next(7);
+                } while (noppa == 0);
+
+                Console.WriteLine(noppa);
+
+                if(noppa==6)
+                {
+                    kutonen++;
+                }  
+            }
+
+            Console.WriteLine($"heitoista {kutonen} oli kutosia.");
+        }
+    }
+}
