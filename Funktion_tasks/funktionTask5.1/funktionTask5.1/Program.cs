8ec1019b3564d9a88ed1512388fc20a611e7cc2b
Tero <tero.pau@gmail.com>
2018-10-16 14:46:36 +0300

    Funktion_tasks initialized, funktionTask5.1 done

---
+++ Funktion_tasks/funktionTask5.1/funktionTask5.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace funktionTask5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintStars();
+            Console.WriteLine();
+        }
+        public static void PrintStars()
+        {
+            Console.Write("Kirjota luku:");
+            string input = Console.ReadLine();
+            int luku;
+            int.TryParse(input, out luku);
+            if (luku < 0)
+            {
+                Console.WriteLine("Syöte on virheellinen.");
+            }
+            else
+            {
+                for (int i = 0; i <luku; i++)
+                {
+                    Console.Write("*");
+                }
+            }
+
+
+        }
+        
+    }
+}
