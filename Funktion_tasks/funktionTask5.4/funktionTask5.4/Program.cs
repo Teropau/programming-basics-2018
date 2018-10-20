6d73d996d8763346466e57fe6abf9979e6ac511d
Tero <tero.pau@gmail.com>
2018-10-20 13:17:58 +0300

    funktionTask 5.4 initialized

---
+++ Funktion_tasks/funktionTask5.4/funktionTask5.4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace funktionTask5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
