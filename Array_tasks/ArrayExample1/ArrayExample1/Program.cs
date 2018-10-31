6865649bb48a7564a5f49645d084c42a402e9384
Tero <tero.pau@gmail.com>
2018-10-31 13:35:53 +0200

    Array_tasks made to be.

---
+++ Array_tasks/ArrayExample1/ArrayExample1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma alustaaa kokonaislukutaulukon (10 alkiota), johon arvotaan lukuja 0...100.");
+            int[] numbers = new int [10000];
+            Random rnd = new Random();
+                //set value to array numbers
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(100);
+            }
+            //Get value from array numbers
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine("Ohjelman suiritus on päättynyt.");
+        }
+    }
+}
---
+++ Array_tasks/arrayTask6.1/arrayTask6.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace arrayTask6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hinnasto");
+            decimal [] productPrice = new decimal[] {7.9m, 72.0m, 99.9m};
+            int [] productAmount = new int[] {1,3,2};
+            decimal[] productTotalPrice = new decimal[3];
+            for (int i = 0; i < productTotalPrice.Length; i++)
+            {
+                productTotalPrice[i] = productAmount[i] * productPrice[i];
+                Console.WriteLine($"Tuote{i+1}. {productTotalPrice[i]:C}");
+            }
+        }
+        
+    }
+}
