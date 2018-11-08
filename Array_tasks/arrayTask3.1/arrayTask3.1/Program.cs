6fe3080ffcc8f7f32d198697c5a06bd6392b4e46
Tero <tero.pau@gmail.com>
2018-11-08 10:49:39 +0200

    arr3.1 Done

---
+++ Array_tasks/arrayTask3.1/arrayTask3.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace arrayTask3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arvotaan 10 lukua ja esitetään ne.");
+            int[] arr = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(20);
+            }
+            for (int j = 0; j < arr.Length; j++)
+            {
+                Console.WriteLine($"[{j}] = {arr[j]:D2}");
+            }
+        }
+    }
+}
